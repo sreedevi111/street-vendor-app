6576e2aa323df5e3ce63794c64882cb279e0ff9e
sreedevi111 <sreedevisobhana@gmail.com>
2022-05-17 14:22:36 +0530

    mobile login screen

--- vendors-app/App.js
+++ vendors-app/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/component/Home';
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Home" component ={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ vendors-app/src/component/Home.js
@@ -0,0 +1,139 @@
+// import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native'
+// import React from 'react'
+// import PhoneInput from 'react-native-phone-number-input'
+
+// const Home = () => {
+//   return (
+//     <View style ={styles.container}>
+//       <Text style = {styles.welcome}>Street Vendor's app</Text>
+//       <SafeAreaView>
+//         <Text>Login</Text>
+//         <PhoneInput style ={styles.phonenum}>
+
+//         </PhoneInput>
+//       </SafeAreaView>
+//     </View>
+    
+//   )
+// }
+
+// export default Home
+
+// const styles = StyleSheet.create({
+//   container:{
+//     flex:1,
+//     padding: 40,
+//     alignItems: "center",
+//     alignItems: "flex-start"
+//   },
+//   welcome:{
+//     color: "blue",
+//     fontWeight: '600',
+//     fontSize: 33
+//   },
+//   phonenum:{
+//     justifyContent: "center",
+//     padding:90,
+//   }
+// })
+
+import React, { useState, useRef } from "react";
+import {
+ SafeAreaView,
+ StyleSheet,
+ View,
+ TouchableOpacity,
+ Text,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import PhoneInput from "react-native-phone-number-input";
+
+const Home = ({ navigation }) => {
+ const [value, setValue] = useState("");
+ const [formattedValue, setFormattedValue] = useState("");
+ const phoneInput = useRef(null);
+
+ return (
+   <>
+     <View style={styles.container}>
+       <SafeAreaView style={styles.wrapper}>
+         <View style={styles.welcome}>
+           <Text>Welcome!</Text>
+         </View>
+         <PhoneInput
+           ref={phoneInput}
+           defaultValue={value}
+           defaultCode="IN"
+           layout="first"
+           onChangeText={(text) => {
+             setValue(text);
+           }}
+           onChangeFormattedText={(text) => {
+             setFormattedValue(text);
+           }}
+           countryPickerProps={{ withAlphaFilter: true }}
+           withShadow
+           autoFocus
+         />
+         <TouchableOpacity
+           style={styles.button}
+           onPress={() => {
+             // TODO - send SMS!
+           }}
+         >
+           <Text style={styles.buttonText}>Sign Up</Text>
+         </TouchableOpacity>
+       </SafeAreaView>
+     </View>
+   </>
+ );
+};
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: Colors.lighter,
+ },
+
+ wrapper: {
+   flex: 1,
+   justifyContent: "center",
+   alignItems: "center",
+ },
+
+ button: {
+   marginTop: 20,
+   height: 50,
+   width: 300,
+   justifyContent: "center",
+   alignItems: "center",
+   backgroundColor: "#7CDB8A",
+   shadowColor: "rgba(0,0,0,0.4)",
+   shadowOffset: {
+     width: 1,
+     height: 5,
+   },
+   shadowOpacity: 0.34,
+   shadowRadius: 6.27,
+   elevation: 10,
+ },
+
+ buttonText: {
+   color: "white",
+   fontSize: 14,
+ },
+
+ welcome: {
+   padding: 20,
+ },
+
+ status: {
+   padding: 20,
+   marginBottom: 20,
+   justifyContent: "center",
+   alignItems: "flex-start",
+   color: "gray",
+ },
+});
+
+export default Home;
